1de866d8f98484990c85b70c72c96a54da1849cc
Piero López <plopezrosas@danielcastelao.org~>
2025-02-12 11:02:01 +0100

    String. Introducción

---
+++ src/Strings.java
@@ -0,0 +1,8 @@
+public class Strings { // Declaro la clase principal
+    public static void main(String[] args) { // Punto de inicio del programa
+        String saludo = "Hola, bienvenido a JAVA"; // Declaro el String
+        String nombre = "Piero";
+
+        System.out.println(saludo + "! " +nombre + "."); // Imprimo por pantalla
+    }
+}
